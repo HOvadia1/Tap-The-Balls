bdea0e12b36bb573b68d9a6516e167972861c3d1
TheLagScript <hunter.ovadia@gmail.com>
2015-03-12 20:09:54 -0400

    Core Game Working [Mouse]
    yayayaya

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,53 +2,88 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
     public GameObject ball;
     public Text timer;
+    public Text ballsLeft;
     public float time;
 
     [HideInInspector]
     public int level;
 
     public int ballModifier;
+    public float timeModifier;
 
     [HideInInspector]
     public int bPL;
+
+    int currentBalls;
 
     void Awake()
     {
         level = 1;
         bPL = level * ballModifier;
-        time = bPL * 2f;
+        time = bPL * timeModifier;
+    }
+
+    public void DrawBalls()
+    {
+        for (int i = 0; i < bPL; i++)
+        {
+            GameObject b = (GameObject)Instantiate(ball, new Vector3(Random.Range(-3.5f, 3.5f), Random.Range(-1.7f, 2.77f), 0.0f), Quaternion.identity);
+            b.name = "ball " + i;
+            b.GetComponent<SpriteRenderer>().color = new Color(Random.value, Random.value, Random.value);
+        }
+        time = bPL * timeModifier;
+
+        currentBalls = bPL;
+    }
+
+    public void NewLevel()
+    {
+        level++;
+        bPL = level * ballModifier;
+        DrawBalls();
     }
 
     void Start()
     {
-        for(int i = 0; i < bPL; i++)
-        {
-            GameObject b = (GameObject) Instantiate(ball, new Vector3(Random.Range(-3.7f, 3.7f), 0.1f, Random.Range(-3, 3)), Quaternion.Euler(new Vector3(90, 0, 0)));
-            b.name = "ball " + i;
-            b.GetComponent<SpriteRenderer>().color = new Color(Random.value, Random.value, Random.value);
-        }
+        DrawBalls();
     }
 
     void Update()
     {
+        bPL = level * ballModifier;
+
         time -= Time.deltaTime;
 
         float seconds = time % 60;
         if (time > 0)
             timer.text = string.Format("{0:00}", seconds);
 
+        //Mouse Logic
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-            if(hit != null && hit.collider.tag == "ball")
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+            if(hit.collider)
             {
-                
+                if(hit.collider.tag == "ball")
+                {
+                    Destroy(hit.collider.gameObject);
+                    currentBalls--;
+                }
             }
         }
+
+        if(currentBalls == 0)
+        {
+            NewLevel();
+        }
+
+        ballsLeft.text = "Balls Left: " + currentBalls;
     }
-    
 }
